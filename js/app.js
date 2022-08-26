1284a51e19c32fd53d45e5e48cd2bf5864ed5c1a
Derek Janssen <drockjanssen@yahoo.com>
2022-08-25 20:52:13 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,39 @@
-console.log('Hello World')
 
 const alertBanner = document.getElementById("alert");
 
-alertBanner.innerHTML =
-`
-<div class="alert-banner">
-  <p><strong>Alert:</strong> You have unread messages</p>
-</div>
-`
-alertBanner.addEventListener('click', e => {
-  const element = e.target;
-  if (element.classList.contains("alert-banner-close")) {
-    alertBanner.style.display = "none";
-  }
+ // Close Alert banner //
+
+ alertBanner.innerHTML = `
+ <div class="alert-banner">
+ <p><strong>Alert:</strong> You have unread messages</p>
+ <p class="alert-banner-close">x</p>
+ </div>
+ `;
+
+ // Close Alert Banner
+ alertBanner.addEventListener('click', (e)=> {
+   const element = e.target;
+   if (element.classList.contains('alert-banner-close')) {
+     alertBanner.style.display = "none";
+   }
+ });
+
+
+// Messaging Section //
+
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+
+send.addEventListener('click', () => {
+if (user.value === "" && message.value === "") {
+alert("Please fill out user and message fields before sending");
+} else if (user.value === "" ) {
+alert("Please fill out user field before sending");
+} else if (message.value === "" ) {
+alert("Please fill out message field before sending");
+} else {
+alert(`Message successfully sent to: ${user.value}`);
+}
 });
